2ed84b125af292c196835da60e08b02a3d8085f6
TannerDryden <tdd7197@gmail.com>
2022-05-31 21:06:05 -0700

    fixed delimiter

--- productrecomender.py
+++ productrecomender.py
@@ -9,7 +9,12 @@ SUPPORT = 100
 def read_data(file_name):
     record = []
     with open(file_name) as input_file:
-        lines = csv.reader(input_file, " ")
+        lines = csv.reader(input_file, delimiter=' ')
         for row in lines:
             record.append(row[:-1])
         return record
+
+
+if __name__ == '__main__':
+    read_data(IN_DATA)
+    print('running')
